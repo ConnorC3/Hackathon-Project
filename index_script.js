b7c6ee514d1c50057290ab5d1cb4234691cedaaf
CoderManChild <atharvak2025@gmail.com>
2023-07-21 15:01:12 -0400

    fixed search

--- index_script.js
+++ index_script.js
@@ -72,7 +72,51 @@ function createPostElement(
 }
 
 function handleSearch() {
-  // Your existing code for handling search
+  const searchTerm = document
+    .getElementById("search-input")
+    .value.toLowerCase();
+  const postsContainer = document.getElementById("postsContainer");
+  let events = JSON.parse(localStorage.getItem("events")) || [];
+
+  // Clear existing posts to avoid duplication
+  postsContainer.innerHTML = "";
+
+  const filteredEvents = events.filter(
+    (event) =>
+      event.name.toLowerCase().includes(searchTerm) ||
+      event.description.toLowerCase().includes(searchTerm) ||
+      event.location.toLowerCase().includes(searchTerm)
+  );
+
+  const postsPerRow = 3;
+  let currentRow = document.createElement("div");
+  currentRow.classList.add("row");
+
+  // Loop through the filtered events and create post elements
+  filteredEvents.forEach(function (event, index) {
+    const postElement = createPostElement(
+      event.name,
+      event.description,
+      event.location,
+      event.time,
+      event.volunteers || 0 // Pass the number of volunteers to the createPostElement function
+    );
+
+    // Add the post to the current row
+    currentRow.appendChild(postElement);
+
+    // Check if we need to start a new row
+    if ((index + 1) % postsPerRow === 0) {
+      postsContainer.appendChild(currentRow);
+      currentRow = document.createElement("div");
+      currentRow.classList.add("row");
+    }
+  });
+
+  // Check if there are any remaining posts in the current row
+  if (currentRow.childElementCount > 0) {
+    postsContainer.appendChild(currentRow);
+  }
 }
 
 function handleAttendance(eventName) {
